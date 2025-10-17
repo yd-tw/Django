62f5da2ed8e41aa8bdbaa8462a38d2c18fa611f2
yd-tw <yd960528@gmail.com>
2025-10-17 17:01:01 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Django
+
+## 指令
+
+- 建置 Python 環境 `run.bat`
+- 啟動開發伺服器 `python manage.py runserver`
+- 退出 Python venv `deactivate`
---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo off
+
+chcp 65001
+
+echo 正在建立虛擬環境
+python -m venv venv
+echo 已創建虛擬環境
+
+call venv\Scripts\activate
+echo 已啟動虛擬環境
+
+pip install -r requirements.txt
+echo 已安裝依賴環境
+
+echo 虛擬環境已啟動
